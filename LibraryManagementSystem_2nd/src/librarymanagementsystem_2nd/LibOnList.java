e2c05aa15a4d24713cd7783aad272e3a29222669
Reth-Sela <selareth7@gmail.com>
2024-07-06 19:31:54 +0700

    add borrow form

---
+++ LibraryManagementSystem_2nd/src/librarymanagementsystem_2nd/LibOnList.java
@@ -0,0 +1,15 @@
+
+public class LibOnList {
+    private String FirstName;
+    private String LastName;
+    
+    public LibOnList(String FirstName, String LastName) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
+    
+    public String toString() {
+    
+        return FirstName + " " + LastName;
+    }
+}
